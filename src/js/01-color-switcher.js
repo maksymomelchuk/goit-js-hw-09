966c9fc9b9618f68c5b15fe82e9ba9bd02df55a5
maksymomelchuk <omelchukmaksym@gmail.com>
2022-10-12 11:11:16 +0300

    exercise 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+let timer = null;
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', () => {
+  timer = setInterval(
+    () => (refs.body.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  clearInterval(timer);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
